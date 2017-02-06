c91653a5dbbb8857c1caf598330458c11d32c9e2
Benjamin Cooper <benjaminkagancooper@gmail.com>
2017-02-06 00:23:04 -0500

    more updates

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Immutable from 'immutable';
 import SearchBar from './searchBar';
 import Note from './note';
 import DashInstance from './dashInstance';
 
+
+
+
+
 //Callback functions
-var error = function (err, response, body) {
-       console.log('ERROR [%s]', err);
-   };
-var success = function (data) {
-      //  console.log('Data [%s]', data);
-      let all_tweets = JSON.parse(data);
-      for(var tweet in all_tweets.statuses) {
-        console.log(all_tweets.statuses[tweet]);
-      }
-   };
+// var error = function (err, response, body) {
+//        console.log('ERROR [%s]', err);
+//    };
+// var success = function (data) {
+//       //  console.log('Data [%s]', data);
+//       let all_tweets = JSON.parse(data);
+//       for(var tweet in all_tweets.statuses) {
+//         console.log(all_tweets.statuses[tweet]);
+//       }
+//    };
 
 
 
@@ -28,12 +33,147 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      dataSources: Immutable.fromJS({
+        china : {
+          fileName: "china_current.json",
+          displayTitle: "Chinese Yuan",
+          jsonData: {},
+        },
+        dowJones: {
+          fileName: "dowjones_current.json",
+          displayTitle: "Dow Jones Industrial Average",
+          jsonData: {},
+        },
+        euro: {
+          fileName: "euro_current.json",
+          displayTitle: "The Euro",
+          jsonData: {},
+        },
+        fox: {
+          fileName: "fox_current.json",
+          displayTitle: "Fox News Twitter",
+          jsonData: {},
+        },
+        mexico: {
+          fileName: "mexico_current.json",
+          displayTitle: "Mexican Peso",
+          jsonData: {},
+        },
+        nasdaq: {
+          fileName: "nasdaq_current.json",
+          displayTitle: "NASDAQ",
+          jsonData: {},
+        },
+        npr: {
+          fileName: "npr_current.json",
+          displayTitle: "National Public Radio Twitter",
+          jsonData: {},
+        },
+        nyt: {
+          fileName: "nyt_current.json",
+          displayTitle: "New York Times Twitter",
+          jsonData: {},
+        },
+        pound: {
+          fileName: "pound_current.json",
+          displayTitle: "British Pound",
+          jsonData: {},
+        },
+        trump: {
+          fileName: "trump_current.json",
+          displayTitle: "Donald Trump's Twitter",
+          jsonData: {},
+        },
+        wsj: {
+          fileName: "wsj_current.json",
+          displayTitle: "Wall Street Journal Twitter",
+          jsonData: {},
+        },
+      }),
       notes: Immutable.Map(),
+
     };
     this.createNode = this.createNode.bind(this);
     this.onDeleteClick = this.onDeleteClick.bind(this);
     this.updatePostion = this.updatePostion.bind(this);
     this.editChangeText = this.editChangeText.bind(this);
+    this.getData = this.getData.bind(this);
+
+
+    // this.getData();
+
+
+
+  }
+
+
+  componentDidMount(){
+    console.log('mounted')
+
+    console.log(this.state.dataSources)
+
+
+    var _this = this;
+
+    this.state.dataSources.entrySeq().forEach(function(key){
+
+      _this.setState({
+        dataSources: this.state.dataSources.update(key, value => {
+          console.log(value)
+        }))
+      }
+    })
+
+
+
+    // var _this = this;
+    //
+    // for (var key in this.state.dataSources) {
+    //
+    //   axios.get(("../../data/" + this.state.dataSources[key].fileName)).then(function(data) {
+    //
+    //
+    //   })
+    //
+    //
+    // }
+
+
+    // axios.get("../../data/trump_current.json").then(function(result) {
+    //   _this.setState({
+    //     twitterData: {result}
+    //   })
+    // });
+
+
+
+
+
+
+  }
+
+  componentWillUnmount(){
+    console.log("COMPONENTWILLUNMOUNT!")
+    // this.serverRequest.abort();
+  }
+
+
+  getData(){
+    console.log('getting data')
+
+
+    axios.get("../../data/trump_current.json").then(function(result){
+      console.log(result.data);
+      var test = result.data;
+      this.updateData(test);
+
+
+    });
+    console.log(2)
+
+
+    console.log(this.state.twitterData)
+
   }
 
 
@@ -45,6 +185,7 @@ class App extends Component {
 
 
   createNode(text) {
+
     const id = uuid.v1();
     const note = {
       title: text,
